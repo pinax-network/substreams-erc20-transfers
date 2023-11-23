5987afbc0af0f0f3a8bbd5f29645befa1751b10d
Mathieu Lefebvre <49442766+Matlefebvre1234@users.noreply.github.com>
2023-11-23 15:49:11 -0500

    schema.sql

---
+++ schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS Transfers  (
+    address FixedString(40),
+    from String,
+    to String,
+    value: String
+    transaction: String
+)
+ENGINE = ReplacingMergeTree()
+ORDER BY (address,from,to)
