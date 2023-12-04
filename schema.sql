78ae036e6c9432459d999ae75a05138bec05c088
Mathieu Lefebvre <49442766+Matlefebvre1234@users.noreply.github.com>
2023-12-04 15:40:33 -0500

    update schema for optimisation

--- schema.sql
+++ schema.sql
@@ -3,7 +3,35 @@ CREATE TABLE IF NOT EXISTS Transfers  (
     `from` String,
     `to` String,
     value String,
-    transaction String
+    transaction String,
+    chain           LowCardinality(String),
+    block_number    UInt32(),
+    timestamp       DateTime64(3, 'UTC'),
 )
-ENGINE = ReplacingMergeTree()
-ORDER BY (address, `from`, `to`);
+ENGINE = MergeTree()
+ORDER BY (timestamp, block_number, chain);
+
+-- Indexes for block_number and chain --
+ALTER TABLE Transfers ADD INDEX transfers_block_number_index block_number TYPE minmax;
+ALTER TABLE Transfers ADD INDEX transfers_chain_index chain TYPE minmax;
+
+-- MV for contract --
+CREATE MATERIALIZED VIEW mv_transfers_contract
+ENGINE = MergeTree()
+ORDER BY (address, `from`,`to`)
+POPULATE
+AS SELECT * FROM Transfers;
+
+-- MV for from --
+CREATE MATERIALIZED VIEW mv_transfers_from
+ENGINE = MergeTree()
+ORDER BY (`from`, address)
+POPULATE
+AS SELECT * FROM Transfers;
+
+-- MV for from --
+CREATE MATERIALIZED VIEW mv_transfers_to
+ENGINE = MergeTree()
+ORDER BY (`to`, address)
+POPULATE
+AS SELECT * FROM Transfers;
