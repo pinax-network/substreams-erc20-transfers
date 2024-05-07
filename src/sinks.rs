44bf8b5c1121b0ae86bb4eeeaec1a6cffd540b9d
Mathieu Lefebvre <49442766+Matlefebvre1234@users.noreply.github.com>
2024-05-07 19:27:19 -0400

    Add db-out

--- src/sinks.rs
+++ src/sinks.rs
@@ -1,7 +1,7 @@
 use substreams::errors::Error;
 use substreams_entity_change::pb::entity::EntityChanges;
 use substreams_entity_change::tables::Tables;
-
+use substreams_database_change::pb::database::{DatabaseChanges, table_change::Operation};
 use crate::pb::erc20::types::v1::TransferEvents;
 
 
@@ -25,3 +25,27 @@ pub fn graph_out(transfers: TransferEvents) -> Result<EntityChanges, Error> {
     }
     Ok(tables.to_entity_changes())
 }
+
+#[substreams::handlers::map]
+pub fn db_out(transfers: TransferEvents) -> Result<DatabaseChanges, Error> {
+
+    // Initialize Database Changes container
+    let mut database_changes: DatabaseChanges = Default::default();
+
+    
+    for event in transfers.transfers {
+        let id = format!("{}-{}", event.block_index, event.transaction);
+
+    // Create row 
+    database_changes.push_change("Transfer", id, 0, Operation::Create)
+        .change("address", (None, event.address))
+        .change("from", (None, event.from))
+        .change("to", (None, event.to))
+        .change("value", (None, event.value))
+        .change("transaction", (None, event.transaction));
+    }
+
+    Ok(database_changes)
+}
+
+
